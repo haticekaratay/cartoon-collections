02c78a902b570a31aa278ab7dbeb1ac1711ca25b
Hatice <haticekaraty@gmail.com>
2021-01-18 13:32:19 -0500

    commit tree 01e6c2eae196f41b6cf30f811ead211299151296

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |name,idx| puts "#{idx+1}.#{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  result_array = []
+  array.map {|word| result_array.push(word.capitalize()+ "!")}
+  result_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+    array.find {|word| 
+    if word.length > 4
+      return true
+    end}
+    return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+cheese_types = ["cheddar", "gouda", "camembert"]
+   array.find { |ingredient| cheese_types.include?(ingredient)}
 end
+
